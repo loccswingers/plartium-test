457732d6a49155275548d4bebfb51c30c0ab165a
loccswingers <144750641+loccswingers@users.noreply.github.com>
2023-10-26 20:55:54 -0500

    replaced

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
-"use strict";
-/**
- * @type {HTMLFormElement}
- */
-const form = document.getElementById("uv-form");
-/**
- * @type {HTMLInputElement}
- */
-const address = document.getElementById("uv-address");
-/**
- * @type {HTMLInputElement}
- */
-const searchEngine = document.getElementById("uv-search-engine");
-/**
- * @type {HTMLParagraphElement}
- */
-const error = document.getElementById("uv-error");
-/**
- * @type {HTMLPreElement}
- */
-const errorCode = document.getElementById("uv-error-code");
+document.querySelector('.dipform').addEventListener('submit', (e) => {
+	e.preventDefault();
 
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-
-  try {
-    await registerSW();
-  } catch (err) {
-    error.textContent = "Failed to register service worker.";
-    errorCode.textContent = err.toString();
-    throw err;
-  }
-
-  const url = search(address.value, searchEngine.value);
-  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+	worker().then(e=>{
+		var val = document.querySelector('.dipinput').value;
+		if (!val.startsWith('http')) val = 'https://' + val;
+	
+		location.assign(window.__DIP.config.prefix + window.__DIP.encodeURL(val));
+	});
 });
+
+document.querySelector('.uvform').addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	worker().then(e=>{
+		var val = document.querySelector('.uvinput').value;
+		if (!val.startsWith('http')) val = 'https://' + val;
+	
+		location.assign(window.__uv$config.prefix + window.__uv$config.encodeUrl(val));
+	});
+});
+
+async function worker() {
+	var a = await navigator.serviceWorker.register('/sw.js', {scope: '/service'});
+	return a;
+}
--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-importScripts('/uv/uv.sw.js');
+importScripts('./dip/dip.worker.js');
+importScripts('./uv/uv.sw.js');
 
 const sw = new UVServiceWorker();
+const sw2 = new DIPServiceWorker('./dip/dip.worker.js');
 
-self.addEventListener('fetch', event =>
-    event.respondWith(
-        sw.fetch(event)
-    )
-);
+self.addEventListener('fetch', event => {
+  if (event.request.url.startsWith(location.origin+'/service/dip/')) event.respondWith(sw2.fetch(event));
+  if (event.request.url.startsWith(location.origin+'/service/uv/')) event.respondWith(sw.fetch(event));
+});
